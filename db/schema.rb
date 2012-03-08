e1cdc01d6f9799a538de82c2f663dc288706d19b
Chris Yuska <chrisyuska@gmail.com>
2012-03-08 18:18:35 -0500

    merged with maxwell

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120308213926) do
+ActiveRecord::Schema.define(:version => 20120308215936) do
 
   create_table "legs", :force => true do |t|
     t.string   "location"
